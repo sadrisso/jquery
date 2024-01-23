534783865000b8a530e9e0089a39515e9d0b608f
sadrisso <sadrisso2000@gmail.com>
2024-01-23 12:20:33 +0600

    first commit

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ let variableOne = $('<p></p>').text('Hi');
 $('#p1').before(variableOne);
 
 $('.container .h1').css({'color': 'red', 'background-color': 'green'});
+
+
+// document.querySelector('.btn').addEventListener('click', function () {
+//     document.querySelector('.h2').innerHTML = 'clicked';
+// });
+
+$('.btn').click(function(){
+    $('.h2').text('clicked');
+})
+
+$('.myButton').click(function() {
+    let value = this.innerHTML;
+    $('.h3').text(value + 'is clicked');
+})
