4117633379ffd3b381ee8cde8a8fbc88699fbebd
sadrisso <sadrisso2000@gmail.com>
2024-01-24 12:47:23 +0600

    calculation functions

--- index.js
+++ index.js
@@ -42,12 +42,26 @@ $('.submitButton').click(function() {
 
 //jQuery for calculator
 
+let resultField = $('#calHead');
+
 function insertNumber(num) {
 
-    let existingNumber = $('#calHead').val();
-    $('#calHead').val(existingNumber + num);
+    let existingNumber = resultField.val();
+    resultField.val(existingNumber + num);
 }
 
 function clearResult() {
-    $('#calHead').val("");
+    resultField.val("");
+}
+
+function calculate() {
+    let result = eval(resultField.val());
+    resultField.val(result);
+}
+
+function deleteResult() {
+    let presentValue = resultField.val();
+    if(presentValue != ''){
+        resultField.val(presentValue.slice(0, -1));
+    }
 }
