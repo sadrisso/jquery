f3e26886fb57aa15367787adf0bfe1fba583be92
sadrisso <sadrisso2000@gmail.com>
2024-01-24 11:29:09 +0600

    cal functionality

--- index.js
+++ index.js
@@ -13,11 +13,41 @@ $('.container .h1').css({'color': 'red', 'background-color': 'green'});
 //     document.querySelector('.h2').innerHTML = 'clicked';
 // });
 
-$('.btn').click(function(){
+$('.btnn').click(function(){
     $('.h2').text('clicked');
 })
 
 $('.myButton').click(function() {
     var value = this.innerHTML;
-    $('.h3').text(value + '' + 'is clicked');
+    $('.h3').text(value + ' is clicked');
 })
+
+$('.submitButton').click(function() {
+
+    let value1 = $('#pass1').val();
+    let value2 = $('#pass2').val();
+
+    if(value1 != '' && value2 != ''){
+        if(value1 == value2){
+            alert('Successfuly login')
+        }else{
+            alert('Password mismatch')
+        }
+    }else{
+        alert('Please enter a password')
+    }
+
+})
+
+
+//jQuery for calculator
+
+function insertNumber(num) {
+
+    let existingNumber = $('#calHead').val();
+    $('#calHead').val(existingNumber + num);
+}
+
+function clearResult() {
+    $('#calHead').val("");
+}
